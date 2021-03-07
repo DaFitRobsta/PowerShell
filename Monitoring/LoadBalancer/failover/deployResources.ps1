13913a859c10b870821077472f856790f71d747d
Robert Lightner <rolightn@microsoft.com>
2021-03-07 16:52:55 -0700

    Updated gov parameters

--- Monitoring/LoadBalancer/failover/deployResources.ps1
+++ Monitoring/LoadBalancer/failover/deployResources.ps1
@@ -4,4 +4,4 @@ New-AzResourceGroupDeployment `
   -Name linkedTemplatesSLBAlerts `
   -ResourceGroupName "RG-RL-USGAZ-DMO-01" `
   -TemplateUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.json" `
-  -TemplateParameterFile 
+  -TemplateParameterUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.parameters.gov.json"
