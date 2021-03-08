7b0478d3c2a7b0b0c2079f11783b6c6ea1b392c2
Robert Lightner <rolightn@microsoft.com>
2021-03-07 20:04:30 -0700

    Added another step to Visio

--- Monitoring/LoadBalancer/failover/deployResources.ps1
+++ Monitoring/LoadBalancer/failover/deployResources.ps1
@@ -1,13 +1,15 @@
 New-AzResourceGroup -Name "RG-RL-USGAZ-DMO-01" -Location usgovarizona
 
+# Azure US Government
 New-AzResourceGroupDeployment `
   -Name linkedTemplatesSLBAlerts `
   -ResourceGroupName "RG-RL-USGAZ-DMO-01" `
   -TemplateUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.json" `
   -TemplateParameterUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.parameters.gov.json"
 
-  New-AzResourceGroupDeployment `
-  -Name linkedTemplatesSLBAlerts `
-  -ResourceGroupName "rl-wu2-dmo-np-rg01" `
-  -TemplateUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.json" `
-  -TemplateParameterUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.parameters.json"
+# Azure Cloud
+New-AzResourceGroupDeployment `
+    -Name linkedTemplatesSLBAlerts `
+    -ResourceGroupName "rl-wu2-dmo-np-rg01" `
+    -TemplateUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.json" `
+    -TemplateParameterUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.parameters.json"
