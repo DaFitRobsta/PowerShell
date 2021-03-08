05ad083899940ef72c0ee6e4ebdad6e83a0001a2
Robert Lightner <rolightn@microsoft.com>
2021-03-07 19:35:43 -0700

    adding commercial test

--- Monitoring/LoadBalancer/failover/deployResources.ps1
+++ Monitoring/LoadBalancer/failover/deployResources.ps1
@@ -5,3 +5,9 @@ New-AzResourceGroupDeployment `
   -ResourceGroupName "RG-RL-USGAZ-DMO-01" `
   -TemplateUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.json" `
   -TemplateParameterUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.parameters.gov.json"
+
+  New-AzResourceGroupDeployment `
+  -Name linkedTemplatesSLBAlerts `
+  -ResourceGroupName "rl-wu2-dmo-np-rg01" `
+  -TemplateUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.json" `
+  -TemplateParameterUri "https://raw.githubusercontent.com/DaFitRobsta/PowerShell/master/Monitoring/LoadBalancer/failover/maintemplate.parameters.json"
